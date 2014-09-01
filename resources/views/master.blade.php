a2e05e28b0387daf1dd61929d478776f5dc26f2d
Usman Riaz <usm4n@users.noreply.github.com>
2014-09-01 23:33:18 +0500

    wrong title fixed

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>User Registration and Sending Emails in Laravel</title>
+    <title>Laravel 4.3: New Awesome Form Validation</title>
     {{HTML::style('assets/css/bootstrap.css')}}
     {{HTML::style('assets/css/style.css')}}
 </head>
