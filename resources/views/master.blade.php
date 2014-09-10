d985d2f5de398a652f93ed6759e93546012d0159
Usman Riaz <usm4n@users.noreply.github.com>
2014-09-10 22:48:40 +0500

    replaced {{ }} with {!! !!} for form output

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel 4.3: New Awesome Form Validation</title>
-    {{HTML::style('assets/css/bootstrap.css')}}
-    {{HTML::style('assets/css/style.css')}}
+    {!!HTML::style('assets/css/bootstrap.css')!!}
+    {!!HTML::style('assets/css/style.css')!!}
 </head>
 <body>
 <div class="container">
