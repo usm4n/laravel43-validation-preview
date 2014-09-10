8043cbc7817b0869fcc7f1b5a6e6f6e9e029e530
Usman Riaz <usm4n@users.noreply.github.com>
2014-09-10 22:48:05 +0500

    replaced {{ }} with {!! !!} for form output

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,46 +3,46 @@
         <h3 class="heading">Laravel 4.3 Validation</h3>
         @if(Session::has('success'))
             <div class="alert alert-success">
-                <p>{{Session::get('success')}}</p>
+                <p>{!!Session::get('success')!!}</p>
             </div>
         @endif
             @foreach($errors->all() as $error)
-            <p class="alert alert-danger">{{$error}}</p>
+            <p class="alert alert-danger">{!!$error!!}</p>
             @endforeach
-        {{Form::open(['route'=>['user.save'],'class'=>'form form-horizontal','style'=>'margin-top:50px'])}}
+        {!!Form::open(['route'=>['user.save'],'class'=>'form form-horizontal','style'=>'margin-top:50px'])!!}
         <div class="form-group">
-            {{ Form::label('username','User Name:',['class'=>'col-sm-3 control-label']) }}
+            {!! Form::label('username','User Name:',['class'=>'col-sm-3 control-label']) !!}
             <div class="col-sm-8">
-                {{ Form::text('username',Input::old('username'),['class'=>'form-control']) }}
+                {!! Form::text('username',Input::old('username'),['class'=>'form-control']) !!}
             </div>
         </div>
         <div class="form-group">
-            {{ Form::label('age','Age:',['class'=>'col-sm-3 control-label']) }}
+            {!! Form::label('age','Age:',['class'=>'col-sm-3 control-label']) !!}
             <div class="col-sm-8">
-                {{ Form::text('age',Input::old('age'),['class'=>'form-control']) }}
+                {!! Form::text('age',Input::old('age'),['class'=>'form-control']) !!}
             </div>
         </div>
         <div class="form-group">
-            {{ Form::label('email','User Email:',['class'=>'col-sm-3 control-label']) }}
+            {!! Form::label('email','User Email:',['class'=>'col-sm-3 control-label']) !!}
             <div class="col-sm-8">
-                {{ Form::text('email',Input::old('email'),['class'=>'form-control']) }}
+                {!! Form::text('email',Input::old('email'),['class'=>'form-control']) !!}
             </div>
         </div>
         <div class="form-group">
-            {{ Form::label('password','Password:',['class'=>'col-sm-3 control-label']) }}
+            {!! Form::label('password','Password:',['class'=>'col-sm-3 control-label']) !!}
             <div class="col-sm-8">
-                {{ Form::password('password',['class'=>'form-control']) }}
+                {!! Form::password('password',['class'=>'form-control']) !!}
             </div>
         </div>
         <div class="form-group">
-            {{ Form::label('password_confirmation','Confirm Password:',['class'=>'col-sm-3 control-label']) }}
+            {!! Form::label('password_confirmation','Confirm Password:',['class'=>'col-sm-3 control-label']) !!}
             <div class="col-sm-8">
-                {{ Form::password('password_confirmation',['class'=>'form-control']) }}
+                {!! Form::password('password_confirmation',['class'=>'form-control']) !!}
             </div>
         </div>
         <div class="text-center">
-            {{Form::submit('Register',['class'=>'btn btn-default'])}}
+            {!!Form::submit('Register',['class'=>'btn btn-default'])!!}
         </div>
-        {{Form::close()}}
+        {!!Form::close()!!}
     </div>
 @stop
